1b1549b7788776df05dd8889a321e43bd46de84f
bhishekg <goabhishek833@gmail.com>
2023-11-17 20:24:16 +0530

    nineth project

---
+++ script.js
@@ -0,0 +1,18 @@
+function generatePassword() {
+    const length = 12;
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=<>?";
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById("password").value = password;
+}
+function copyToClipboard() {
+    const passwordField = document.getElementById("password");
+    passwordField.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
